5474fe212c30ca948e2490b844172c61f7db3bd3
Bruno <brunoomiguel10@hotmail.com>
2023-10-03 15:05:08 +0100

    Makefile fixed

--- Makefile
+++ Makefile
@@ -3,32 +3,32 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: brpereir <brpereir@student.42.fr>          +#+  +:+       +#+         #
+#    By: brunolopes <brunolopes@student.42.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/08/08 19:05:23 by brpereir          #+#    #+#              #
-#    Updated: 2023/09/05 17:05:44 by brpereir         ###   ########.fr        #
+#    Updated: 2023/10/03 15:04:50 by brunolopes       ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = push_swap
 
-SRC = src/main.c src/operations.c src/operations2.c src/operations3.c src/utils.c
-OBJ = ${SRC:.c=.o}
+CFLAGS = -Wall -Wextra -Werror
+SRC = src/main.c src/operations.c src/operations2.c src/operations3.c src/utils.c src/utils2.c src/simple.c src/simple2.c src/radix.c
+OBJS = ${SRC:.c=.o}
 
 RM = rm -f
 CFLAGS = -Wall -Wall -Wextra
-LIBFT_PATH = libft/libft.a
+
+$(NAME): $(OBJ)
+	cc $(CFLAGS) $(OBJS) -o $(NAME) 
 
 all: $(NAME)
-
-$(NAME): %
+	@echo "Done!"
 
 clean: 
-	make libft clean
-	$(RM) $(OBJ)
+	$(RM) $(OBJS)
 
 fclean: clean
-	make libft clean
 	$(RM) $(NAME) 
 
 re: fclean all
--- src/simple.c
+++ src/simple.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   simple.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brpereir <brpereir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: brunolopes <brunolopes@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 15:46:17 by brpereir          #+#    #+#             */
-/*   Updated: 2023/09/19 15:12:12 by brpereir         ###   ########.fr       */
+/*   Updated: 2023/10/03 15:03:45 by brunolopes       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 void ft_simple_sort(t_stack *stack_a, t_stack *stack_b)
 {
 	int	size;
-	size = ft_stack_size(&stack_a);
+	size = ft_stack_size(stack_a);
 
 	if(size == 2)
-		ft_sa(&stack_a, 1);
+		ft_sa(stack_a, 1);
 	else if (size == 3)
-		ft_sort_three(&stack_a);
+		ft_sort_three(stack_a);
 	else if (size == 4)
 		ft_sort_four(stack_a, stack_b);
 	else if (size == 5 )
@@ -44,10 +44,10 @@ void ft_sort_three(t_stack *stack_a)
 		ft_rra(&stack_a, 1);
 	}
 	else if ( a > b  && b < c && a > c)
-		ft_ra(stack_a, 1);
+		ft_ra(&stack_a, 1);
 	else if (a < b && b > c && a < c){
 		ft_sa(stack_a, 1);		
-		ft_ra(stack_a, 1);		
+		ft_ra(&stack_a, 1);		
 	}
 	else
 		ft_rra(&stack_a, 1);
@@ -57,7 +57,7 @@ void ft_sort_four(t_stack *stack_a, t_stack *stack_b)
 {
 	int	min;
 	
-	min =  ft_min_value(&stack_a);
+	min =  ft_min_value(*stack_a);
 	ft_min_four(stack_a, stack_b, min);
 	ft_sort_three(stack_a);
 	ft_pa(stack_a, stack_b);
@@ -69,10 +69,10 @@ void ft_sort_five(t_stack *stack_a, t_stack *stack_b)
 
 	if (ft_is_sorted(stack_a))
 		return ;
-	i = ft_min_value(stack_a);
-	min_five(stack_a, stack_b, i);
-	i = ft_min_value(stack_a);
-	min_four(stack_a, stack_b, i);
+	i = ft_min_value(*stack_a);
+	ft_min_five(stack_a, stack_b, i);
+	i = ft_min_value(*stack_a);
+	ft_min_four(stack_a, stack_b, i);
 	ft_sort_three(stack_a);
 	ft_pa(stack_a, stack_b);
 	ft_pa(stack_a, stack_b);
